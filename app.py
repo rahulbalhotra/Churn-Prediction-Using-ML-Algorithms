2dcb30a1174afd92caab9ee8f48281c2f75ff83d
Rahul Balhotra <40921516+rahulbalhotra@users.noreply.github.com>
2022-06-03 20:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import pickle
-model = pickle.load(open("/notebook/model.sav", "rb"))
+model = pickle.load(open("./notebook/model.sav", "rb"))
 
 #Import python scripts
 from preprocessing import preprocess
