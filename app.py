3cdb6b54171a916d7c8e076cf6d2b34325d4f242
Rahul Balhotra <40921516+rahulbalhotra@users.noreply.github.com>
2022-06-03 20:38:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-
+import pickle
 model = pickle.load(open("/notebook/model.sav", "rb"))
 
 #Import python scripts
