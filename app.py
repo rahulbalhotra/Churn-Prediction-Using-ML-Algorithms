d90fb5aa3773f6fb2e7ea751c33356ab8811470c
Rahul Balhotra <40921516+rahulbalhotra@users.noreply.github.com>
2022-06-03 20:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-
+pip install joblib
 #load the model from disk
 import joblib
 model = joblib.load(r"./notebook/model.sav")
